bc0dfe94f88a70f1075f33dac00099d139b79826
AhkamTasin <163498844+AhkamTasin@users.noreply.github.com>
2024-05-25 21:10:11 +0600

    Add files via upload
    added week 10 day 02

---
+++ Week 10/Day 02/Inversion_Count.cpp
@@ -0,0 +1,36 @@
+//Author: Ahkam_Tasin
+//Created: 2024-05-25 16:30:33
+//<a href= />
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long int
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        ll ans=0;
+        pbds<int>p;
+        for(int i=n-1;i>=0;i--)
+        {
+            ans+=p.order_of_key(a[i]);
+            p.insert(a[i]);
+        }
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
