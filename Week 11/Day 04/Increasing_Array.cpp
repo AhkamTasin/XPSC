384ffd56427ef8b7f145bd263bd048f71cc508b0
AhkamTasin <163498844+AhkamTasin@users.noreply.github.com>
2024-06-04 00:13:32 +0600

    Increasing_Array.cpp

--- Week 11/Day 04/Increasing_Array.cpp
+++ Week 11/Day 04/Increasing_Array.cpp
@@ -12,28 +12,22 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
+    ll n;
     cin>>n;
-    vector<int>a(n);
-    for(int i=0;i<n;i++)
+    vector<ll>a(n);
+    for(ll i=0;i<n;i++)
     {
         cin>>a[i];
     }
-    int steps=0;
-    int boro=a[0];
-    int chuto=0;
-    for(int i=1;i<n;i++)
+    ll steps=0;
+    for(ll i=1;i<n;i++)
     {
-        if(a[i]<boro)
+        if(a[i-1]>a[i])
         {
-            boro=a[i-1];
-
             steps+=a[i-1]-a[i];
+            a[i]=a[i-1];
         }
-        else
-        {
-            boro=a[i];
-        }
+        
     }
     cout<<steps<<'\n';
     return 0;
